e49eb25c2efa0139f5b36dd86cb8b476b47a0328
Gayatrix22 <songiregayatri@gmail.com>
2026-04-04 12:17:48 +0530

    slug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",   // 👈 ye add karo
   images: {
     unoptimized: true, // 👈 important for static export
   },
